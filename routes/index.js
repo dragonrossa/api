a9450dfcb175a5b3390a614e56691d7bf3e3f1ee
dragonrossa <rodjuga@gmail.com>
2020-05-21 12:34:01 +0200

    Update of index.pug file

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+config = "http://localhost:3000"
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'main API page',
     route1: __dirname + '\\kitchen', 
     route2: __dirname + '\\users', 
+    config1: config + "/kitchen",
+    config2: config + "/users",
+    link:"www.test.com"
 
   });
 });
